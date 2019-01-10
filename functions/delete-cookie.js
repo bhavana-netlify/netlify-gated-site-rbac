dc14b9e1cd25408df1b91be5df083fd67ae1c4cb
divya <hello@shortdiv.com>
2019-01-10 16:21:08 -0600

    update redirect on delete cookie

--- functions/delete-cookie.js
+++ functions/delete-cookie.js
@@ -1,19 +1,38 @@
 const cookie = require("cookie");
 
 exports.handler = function(event, context, callback) {
+  const redirectToLogin = event.headers.referer;
+
   const netlifyCookie = cookie.serialize("nf_jwt", null, {
     secure: true,
     path: "/",
-    expires: new Date()
+    maxAge: -1
   });
 
+  const html = `
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <noscript>
+        <meta http-equiv="refresh" content="0; url=${redirectToLogin}" />
+      </noscript>
+    </body>
+    <script>
+      setTimeout(function(){
+        window.location.href = ${JSON.stringify(redirectToLogin)}
+      }, 0)
+    </script>
+  </html>`;
+
   callback(null, {
-    statusCode: 302,
+    statusCode: 200,
     headers: {
-      Location: "https://festive-brown-c3df1a.netlify.com",
       "Set-Cookie": netlifyCookie,
-      "Cache-Control": "no-cache"
+      "Cache-Control": "no-cache",
+      "Content-Type": "text/html"
     },
-    body: JSON.stringify({ message: "Cookie has been removed" })
+    body: html
   });
 };
